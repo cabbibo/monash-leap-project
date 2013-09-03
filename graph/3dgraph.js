e0cdfcdf770a301495e90f3df4b5395de77cadf7
Nicholas Smith <nmsmith65@gmail.com>
2013-09-03 13:20:20 +1000

    Added colorings and ability to drag.

--- graph/3dgraph.js
+++ graph/3dgraph.js
@@ -2,11 +2,13 @@
 
 var scene;
 var camera;
+var nearClip = 1, farClip = 100;
 var renderer;
+var mouse = new THREE.Vector2();
 
 function mainLoop()
 {
-  update();
+  nextUpdate();
   draw();
 }
 
@@ -28,7 +30,7 @@ function initializeScene()
 
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 1, 100);
+  camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, nearClip, farClip);
   camera.position.set(0, 0, 50);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
   scene.add(camera);
@@ -70,9 +72,10 @@ function buildGraph()
 // A hash table to store all collected Twitter users
 var users = {};
 var springRestLength = 10;
-var springK = 10;
-var repulsionStrength = 25;
-var dragConstant = 0.05;
+var springK = 30;
+var repulsionStrength = 80;
+var dragConstant = 0.15;
+var mouseDragForce = 20;
 
 function User(name)
 {
@@ -86,6 +89,7 @@ function User(name)
     this.sphereMaterial
   );
 
+  this.sphere.user = name;
   this.sphere.position.set((Math.random()-0.5)*15, (Math.random()-0.5)*15, (Math.random()-0.5)*15-10);
   scene.add(this.sphere);
   this.velocity = new THREE.Vector3();
@@ -95,18 +99,27 @@ function User(name)
   this.followerEdges = new Array();
 }
 
-User.prototype.sphereMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
+User.prototype.sphereMaterial = new THREE.MeshLambertMaterial({color: 0x00F2FF});
 
 User.prototype.drawNewEdges = function() {
   for (var i = 0; i < this.followerEdges.length; ++i) {
     this.followerEdges[i].remove();
   }
   this.followerEdges = new Array();
-  for (var i = 0; i < this.followers.length; ++i) {
+  NEXT_FOLLOWER: for (var i = 0; i < this.followers.length; ++i) {
     var follower = User[this.followers[i]];
 
-    if (follower !== undefined)
+    if (follower !== undefined) {
+      var edges = follower.followerEdges;
+      // If the person we're following is following us, update the existing edge
+      for (var j = 0; j < edges.length; ++j) {
+        if (edges[j].follower === this) {
+          edges[j].lineGeo.colors[1].setHex(0xFF0000);
+          continue NEXT_FOLLOWER;
+        }
+      }
       this.followerEdges[this.followerEdges.length] = new Edge(this, follower);
+    }
   }
 }
 
@@ -136,6 +149,17 @@ User.prototype.calculateForces = function() {
       this.accel.add(displacement);
     }
   }
+
+  // Add force from being dragged around via interaction
+  if (this.selected) {
+    var mouseRay = new THREE.Vector3(mouse.x, mouse.y, 1);
+    projector.unprojectVector(mouseRay, camera);
+    mouseRay.sub(camera.position);
+    var factor = (this.sphere.position.z - camera.position.z) / mouseRay.z;
+    var mousePosition = camera.position.clone().add(mouseRay.multiplyScalar(factor));
+    var displacement = mousePosition.sub(this.sphere.position);
+    this.accel.add(displacement.multiplyScalar(mouseDragForce));
+  }
 }
 
 User.prototype.updatePosition = function(deltaTime) {
@@ -163,11 +187,15 @@ function Edge(followee, follower)
   this.lineGeo.dynamic = true;
   this.lineGeo.vertices.push(followee.sphere.position);
   this.lineGeo.vertices.push(follower.sphere.position);
+  this.lineGeo.colors.push(new THREE.Color(0xFF0000));
+  this.lineGeo.colors.push(new THREE.Color(0x0000FF));
   this.lineMesh = new THREE.Line(this.lineGeo, this.lineMaterial);
   scene.add(this.lineMesh);
 }
 
-Edge.prototype.lineMaterial = new THREE.LineBasicMaterial({color: 0xFFFFFF});
+Edge.prototype.lineMaterial = new THREE.LineBasicMaterial(
+  {color: 0xFFFFFF, vertexColors: THREE.VertexColors}
+);
 Edge.prototype.update = function() {
   this.lineGeo.verticesNeedUpdate = true;
 }
@@ -175,14 +203,24 @@ Edge.prototype.remove = function() {
   scene.remove(this.lineMesh);
 }
 
+var frameTimeLimit = 0.05;
 var timeOfLastFrame = 0;
 
-function update()
+function nextUpdate()
 {
   var currentTime = new Date().getTime();
   var deltaTime = (currentTime - timeOfLastFrame)/1000;
   timeOfLastFrame = currentTime;
 
+  // Limit the maximum time step to avoid unexpected results
+  if (deltaTime > frameTimeLimit)
+    update(frameTimeLimit);
+  else
+    update(deltaTime);
+}
+
+function update(deltaTime)
+{
   for (var p in User) {
     User[p].calculateForces();
   }
@@ -198,11 +236,63 @@ function draw()
   renderer.render(scene, camera);
 }
 
+var selectedUser = null;
+var projector = new THREE.Projector();
+document.addEventListener('mousedown',
+                        function(event) {
+                          var directionVector = new THREE.Vector3(mouse.x, mouse.y, 1);
+                          projector.unprojectVector(directionVector, camera);
+                          directionVector.sub(camera.position);
+                          directionVector.normalize();
+
+                          var raycaster = new THREE.Raycaster(camera.position, directionVector, nearClip, farClip);
+                          var intersected = raycaster.intersectObjects(scene.children);
+
+                          for (var i = 0; i < intersected.length; ++i) {
+                            if (intersected[i].object.user !== undefined) {
+                              selectedUser = User[intersected[i].object.user];
+                              selectedUser.selected = true;
+                              break;
+                            }
+                          }
+                        }
+                       );
+document.addEventListener('mouseup',
+                        function(event) {
+                          if (selectedUser !== null) {
+                            selectedUser.selected = false;
+                            selectedUser = null;
+                          }
+                        }
+                       );
+document.addEventListener('mousemove',
+                          function(event) {
+                            event.preventDefault();
+
+                            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+                            mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+                          },
+                          false);
+window.addEventListener('resize',
+                        function(event) {
+                          renderer.setSize(window.innerWidth, window.innerHeight);
+                          camera.aspect = window.innerWidth/window.innerHeight;
+                          camera.updateProjectionMatrix();
+                        }
+                       );
+
+function printVector(v)
+{
+  console.log(v.x + ", " + v.y + ", " + v.z)
+}
+
 initializeScene();
 buildGraph();
 timeOfLastFrame = new Date().getTime();
-update();
 setInterval(mainLoop, 1000/60);
+
+
+
 
 
 
